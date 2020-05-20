bcb9d56bdf7147c93637138ea6f49a95a76d1b14
Leo Wang <leo.wang@stud.fh-campuswien.ac.at>
2020-05-20 17:53:50 +0200

    include header&footer, using php

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+
+    <a class="grey-text text-lighten-3" href="#!">About Us</a><br>
+    <a class="grey-text text-lighten-3" href="#!">FAQ</a>
+    <div class="copyright">
+        © 2020 Page Turner, Ltd - All Right Reserved.
+    </div>
+</footer>
+            
---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Font - Roboto -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+    <!-- Font - Adobe Heiti Std Medium 500 -->
+    <link href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;500&display=swap" rel="stylesheet">
+    <title>Page Turner</title>
+</head>
