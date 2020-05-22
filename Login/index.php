e3da0e83778bc34b15611ffac09bcd1eb6fd64d2
Leo Wang <leo.wang@stud.fh-campuswien.ac.at>
2020-05-22 20:31:07 +0200

    loginpage added background image

--- Login/index.php
+++ Login/index.php
@@ -1,69 +1,58 @@
-<?php 
+<?php
 require_once 'controllers/authController.php'; //session is started there, other things set here like set Session
 include('C:\xampp\htdocs\page-turner\includes/header.php'); //PLEASE CHANGE PATH
 include('C:\xampp\htdocs\page-turner\includes/nav.php');
 
 
 //verify user using
-if(isset($_GET['token'])){
+if (isset($_GET['token'])) {
     $token = $_GET['token'];
     verifyUser($token);
 }
 
 //we need to check if user not logged in he cannot see the page therefore
-if(!isset($_SESSION['id'])){
+if (!isset($_SESSION['id'])) {
     header('location: login.php');
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>Homepage</title>
-
-    <link rel="stylesheet" href="style.css">
-</head>
 <body>
-    
-<div class="container">
+<div class="container bg">
     <div class="row">
         <div class="col-md-4 offset-md-4 form-div login">
-        
-        <?php if(isset($_SESSION['message'])): ?>
-        <div class="alert <?php echo $_SESSION['alert-class']; ?>"> <!-- call of global variable session -->
-         <?php 
-         echo $_SESSION['message'];
-         unset($_SESSION['message']);
-         unset($_SESSION['alert-class']);
-         
-         ?> 
-        </div>
-        <?php endif; ?>
+            <?php if (isset($_SESSION['message'])): ?>
+                <div class="alert <?php echo $_SESSION['alert-class']; ?>"> <!-- call of global variable session -->
+                    <?php
+                    echo $_SESSION['message'];
+                    unset($_SESSION['message']);
+                    unset($_SESSION['alert-class']);
 
-        <h3>Welcome, <?php echo $_SESSION['username']; ?> </h3>
+                    ?>
+                </div>
+            <?php endif; ?>
 
-        <a href="index.php?logout=1" class="logout">logout</a>
-        
+            <h3>Welcome, <?php echo $_SESSION['username']; ?> </h3>
 
-        <?php if(!$_SESSION['verified']): //if not verified,
-            ?> 
-        <div class="alert alert-warning">
+            <a href="index.php?logout=1"">
+            <button class="btn btn-block btn-lg btn-danger">Logout</button>
+            <br>
+            </a>
 
-            You need to verify your account. Sign in to your email account and click on the
-            verification link we just emailed you at 
-            <strong><?php echo $_SESSION['email']; ?></strong> <!-- call of global variable session -->
-            
-        </div>
-        <?php endif; ?>
+            <?php if (!$_SESSION['verified']): //if not verified,
+                ?>
+                <div class="alert alert-warning">
 
-        <?php if($_SESSION['verified']): //if verified,
-            ?>
-        <button class="btn btn-block btn-lg btn-primary">You are verified!</button>
-        <?php endif; ?>
+                    You need to verify your account. Sign in to your email account and click on the
+                    verification link we just emailed you at
+                    <strong><?php echo $_SESSION['email']; ?></strong> <!-- call of global variable session -->
+
+                </div>
+            <?php endif; ?>
+
+            <?php if ($_SESSION['verified']): //if verified,
+                ?>
+                <div class="alert alert-success" role="alert">You have been verified!</div>
+            <?php endif; ?>
         </div>
     </div>
 </div>
--- Login/login.php
+++ Login/login.php
@@ -1,49 +1,39 @@
-<?php require_once 'controllers/authController.php'; 
+<?php require_once 'controllers/authController.php';
 include('C:\xampp\htdocs\page-turner\includes/header.php');
 include('C:\xampp\htdocs\page-turner\includes/nav.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>Login</title>
-
-    <link rel="stylesheet" href="style.css">
-</head>
 <body>
-    
-<div class="container">
+
+<div class="container bg">
     <div class="row">
         <div class="col-md-4 offset-md-4 form-div login">
-        <form action="login.php" method="post">
-        <h3 class = "text-center">Login</h3>
+            <form action="login.php" method="post">
+                <h3 class="text-center">Login</h3>
+                <?php if (count($errors)): ?>
+                    <div class="alert alert-danger">
+                        <?php foreach ($errors as $error): ?>
+                            <li><?php echo $error; ?></li>
+                        <?php endforeach; ?>
 
-        <?php if(count($errors)):?>
-        <div class="alert alert-danger">
-            <?php foreach($errors as $error): ?>
-            <li><?php echo $error; ?></li>
-            <?php endforeach; ?>
-        
-        </div>
-        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
 
-        <div class="form-group">
-        <label for="username">Username or E-Mail</label>
-        <input type="text" name= "username" value= "<?php echo $username; ?>" class= "form-control form-control-lg">
-        </div>
-        
-        <div class="form-group">
-        <label for="password">Username</label>
-        <input type="password" name= "password" class= "form-control form-control-lg">
-        </div>
-        
-        <div class="form-group">
-            <button type="submit" name="login-btn" class="btn btn-primary btn-block btn-lg">Login</button>
-        </div>
-        <p class= "text-center">Not yet a member? <a href="signup.php">Sign Up</a></p>
+                <div class="form-group">
+                    <label for="username">Username/E-Mail</label>
+                    <input type="text" name="username" value="<?php echo $username; ?>"
+                           class="form-control form-control-lg">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" class="form-control form-control-lg">
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" name="login-btn" class="btn btn-primary btn-block btn-lg">Login</button>
+                </div>
+                <p class="text-center">Not yet a member? <a href="signup.php">Sign Up</a></p>
         </div>
     </div>
 </div>
