5d50db757e0f7569240eb7cc9e21431bd94da97e
Maximilian Stöckl <maximilian@doki.at>
2020-05-26 22:58:31 +0200

    early shoppingcart

--- cart.php
+++ cart.php
@@ -2,20 +2,36 @@
 include $_SERVER['DOCUMENT_ROOT'] . "/page-turner/includes/header.php";
 include $_SERVER['DOCUMENT_ROOT'] . "/page-turner/includes/nav.php";
 ?>
+<body>
+
+
+<div id="container">
+    <h1>Shopping Cart</h1>
+    <hr>
+  <!--  <p id="books"></p-->
+   
+</div>
+</body>
+
+<!doctype html>
+<html lang="en">
+<head>
+<!--shoppingcart js script -->
+<script src="/page-turner/js/shoppingcart.js" type="text/javascript" async></script>
+</head>
 
 <body>
 
-<div id="container">
+   <!--main id="shoppingCart">
+   <main id="mainBookScreen">
+   </main>
 
-    Looks pretty empty in here, try to add some books!
+   <div id="total"></div-->
 
-    <script>
-        addBookToShoppingCart();
-    </script>
-
-</div>
 </body>
+ </html>
+
+
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/page-turner/includes/footer.php";
 ?>
-
