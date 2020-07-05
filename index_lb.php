fa8b4f8905928d1b3e5e5373f44de7dae16ee489
sandravrdoljak <sandra.vrdoljak@stud.fh-campuswien.ac.at>
2020-07-05 16:45:17 +0200

    pagination deleted

--- index_lb.php
+++ index_lb.php
@@ -18,32 +18,6 @@ include $_SERVER['DOCUMENT_ROOT'] . "/page-turner/includes/nav.php";
         getEBookLists(9781453286142); //Playing for Keeps
     </script>
 
-    <!-- Pagination -->
-    <ul class="pagination justify-content-center">
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-                <span class="sr-only">Previous</span>
-            </a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-                <span class="sr-only">Next</span>
-            </a>
-        </li>
-    </ul>
-
-
 </div>
 </body>
 <?php
